80c8acb29402bc18303d47d4f03372d772e64523
letrananhtoankts <letrananhtoan.kts@gmail.com>
2026-08-21 11:24:27 +0700

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -6,7 +6,7 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // Lấy ở Supabase → Settings → API
 export const SUPABASE_URL = 'https://gvhwjemqarnevatwuktq.supabase.co';
-export const SUPABASE_ANON_KEY = 'DÁN_PUBLISHABLE_KEY_VÀO_ĐÂY'; // sb_publishable_... — KHÔNG dùng secret key ở đây
+export const SUPABASE_ANON_KEY = 'sb_publishable_5sf273CSlrsTx4-1RFyl3g_xxvIjh-b'; // sb_publishable_... — KHÔNG dùng secret key ở đây
 
 export const APP_NAME = 'VELA Hồ Sơ TC';
 export const APP_BASE_URL = 'https://velae-c.github.io/VELA_HSTC';
